a23b1b39d7853e206b91a198947f60e58826d819
Jabneel <zjabneel@gmail.com>
2019-12-20 10:26:00 -0500

    removed service worker

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return(
-		<div style={{overflow: 'scroll', border: '3px solid black', height: '500px'}}>
+		<div style={{overflow: 'scroll', border: '3px solid black', height: '800px'}}>
 			{props.children}
 		</div>
 	)
